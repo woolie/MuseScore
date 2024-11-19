beee8ca07e76d9e11a4caee9c455d56d6174a9b9
Michele Spagnolo <m.spagnolo@mu.se>
2024-11-19 12:01:19 +0100

    System locks

--- src/engraving/dom/measure.cpp
+++ src/engraving/dom/measure.cpp
@@ -1379,6 +1379,15 @@ bool Measure::acceptDrop(EditData& data) const
         case ActionIconType::STAFF_TYPE_CHANGE:
             viewer->setDropRectangle(staffRect);
             return true;
+        case ActionIconType::SYSTEM_LOCK:
+        {
+            if (score()->layoutMode() == LayoutMode::PAGE) {
+                const System* sys = system();
+                viewer->setDropRectangle(sys->canvasBoundingRect().adjusted(sys->leftMargin(), 0.0, 0.0, 0.0));
+                return true;
+            }
+            return false;
+        }
         default:
             break;
         }
@@ -1715,6 +1724,9 @@ EngravingItem* Measure::drop(EditData& data)
             score()->undoAddElement(stc);
             break;
         }
+        case ActionIconType::SYSTEM_LOCK:
+            score()->toggleSystemLock({ system() });
+            break;
         default:
             break;
         }
